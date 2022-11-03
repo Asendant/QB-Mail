b17d69c2a8c8c9d85dad94c779b1477c07b8caa7
Jacob <91891584+Asendant@users.noreply.github.com>
2022-11-03 07:29:30 -0700

    Add files via upload

---
+++ html/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('.container').hide();
+
+    window.addEventListener('message', function (event) {
+        let data = event.data;
+
+        if (data.type === "ui") {
+            if (data.display === true) {
+                $('.container').show();
+            } else {
+                $('.container').hide();
+            }
+        }
+    });
+
+    $('#submit').click(function () {
+        GetSubmittedAmount();
+    })
+})
+
+function GetSubmittedAmount() {
+    let value = parseInt(document.getElementById('packageAmount').value);
+
+    if (value <= 0 || value > 255) {
+        $.post(`https://${GetParentResourceName()}/error`, JSON.stringify({
+            error: "Invalid Input.  Needs to be between 1 and 255."
+        }))
+
+        return;
+    }
+
+    $('.container').hide();
+
+    $.post(`https://${GetParentResourceName()}/setPackageAmount`, JSON.stringify({
+        amount: value
+    }))
+
+    return;
+}
